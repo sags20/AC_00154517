685df9607d601d9bd2caf6d1b557a6ffa8c3c99f
sags20 <00154517@uca.edu.sv>
2021-06-15 22:00:55 -0600

    ejercicio final

--- tarea_laboratorio_5/ejercicio.asm
+++ tarea_laboratorio_5/ejercicio.asm
@@ -9,34 +9,34 @@ org 100h
 
     MOV SI, 0
     MOV DI, 0d
-
+    
     ;Fila y columana para mostrar el cursor
-    MOV byte [200h], 10 
-    MOV DL, 20 
+    MOV DH, 10 
+    MOV DL, 20d
 
     call modotexto
 
+;Iteracion principal
     iterar:
     ;Mover el cursor y el caracter a imprimir en pantalla
         call movercursor 
         ;Colocar en CL el caracter actual 
         MOV CL, [cadena+SI] 
         call escribircaracter
-        
         INC SI 
-        INC DL 
-        
+        INC DL
         ;Contador para terminar la ejecución del programa 
         INC DI 
-        CMP DI, 10d
-        ;Si DI es menor a 20, entonces que siga iterando. 
-        JB iterar 
-        jmp tecla 
+        CMP DI, 35d 
+        ;Si DI es menor a 35, entonces que siga iterando. 
+        JB iterar
+        ;Caso contrario, que salte a esperar tecla.
+        jmp tecla
 
     modotexto: 
         ;Activa modo texto
         MOV AH, 0h 
-        ;Modo gráfico 
+        ;Modo gráfico deseado
         MOV AL, 03h 
         INT 10h
         RET
@@ -44,32 +44,43 @@ org 100h
     movercursor:
         ;Posiciona el cursor en pantalla.
         MOV AH, 02h 
-        MOV BH, 0h 
-        MOV DH,[200h]
+        MOV BH, 0h
+
+        ;Primer nombre/apellido
+        CMP DI, 06d
+        JE aumentarFila
+        ;Segundo nombre/apellido
+        CMP DI, 17d
+        JE aumentarFila
+        
         INT 10h
         RET
 
     escribircaracter:
         ;Escribe caracter en pantalla según posición del cursor
         MOV AH, 0Ah 
-        MOV AL, CL
+        MOV AL, CL 
         MOV BH, 0h 
         ;Número de veces a escribir el caracter
         MOV CX, 1h 
         INT 10h
         RET
 
+    ;Funcion que ayuda para recorrer cada nombre y apellido
+    aumentarFila :
+        INC DH
+        MOV DL, 21
+        RET
+
     tecla:
-        ;Se espera buffer del teclado para avanzar en la siguiente instrucción
+        ;Espera buffer del teclado para avanzar en la siguiente instrucción
         MOV AH, 00h 
         INT 16h
 
+    ;Final del programa 
     exit:
         int 20h
 
-
-
     section .data
     ;Txto a mostrar 
-    cadena DB 'hola mundo','hola mundo'
-    
+    cadena DB 'Sofia  Alexandra  Galvez  Salguero'
